37e8dee3525ad8c785c42be79427bcf734f26dd3
Suprita211 <suprita2026@gmail.com>
2025-03-08 13:13:41 +0530

    Update addemployeeform.jsx

--- my-app/src/Components/addemployeeform.jsx
+++ my-app/src/Components/addemployeeform.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 const {API_URL_PROD} = process.env;
-const API_URL = "http://localhost:8080/api/abs";
+const API_URL = "https://ems-be-v1.onrender.com/api/abs";
 const AddEmployeeForm = () => {
   const [formData, setFormData] = useState({
     EmpName: '',
