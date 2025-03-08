326ec666a7e771ed7e4b47b6f6a2c86c5293a51f
Suprita Dutta Roy <suprita213@gmail.com>
2025-03-08 12:51:25 +0530

    ar parchi na

--- Backend/controllers/salaryController.js
+++ Backend/controllers/salaryController.js
@@ -536,6 +536,20 @@ const generateSalary = async (req, res) => {
 
       const bankData = await BankSchema.findOne({ EmpID: employee.EmpID });
 
+      function toFixed(value, precision) {
+        var precision = precision || 0,
+            power = Math.pow(10, precision),
+            absValue = Math.abs(Math.round(value * power)),
+            result = (value < 0 ? '-' : '') + String(Math.floor(absValue / power));
+    
+        if (precision > 0) {
+            var fraction = String(absValue % power),
+                padding = new Array(Math.max(precision - fraction.length, 0) + 1).join('0');
+            result += '.' + padding + fraction;
+        }
+        return result;
+    }
+
       // Generate HTML for PDF
       const htmlContent = `
                                <!DOCTYPE html>
@@ -865,14 +879,14 @@ const generateSalary = async (req, res) => {
           <tr class="tr">
           <td></td><td></td><td></td><td></td><td></td></tr>
             <td><strong>&nbsp;Net Salary(₹)</strong></td>
-            <td colspan="4">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;${
-              Math.round(salaryData.netIncome * 100) / 100
+            <td colspan="4">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;${
+              (toFixed(salaryData.netIncome,0))
             } 
             </td>
           </tr>
           <tr>
           <td colspan="5">Amount in words: ${numberToWords(
-            salaryData.netIncome
+            toFixed(salaryData.netIncome,0)
           )}</td>
           </tr>
 
